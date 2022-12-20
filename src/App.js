7204dba7f7571b7c9b200ed0ac1be61d4ee5ecf5
dedikurniawanx <dedikurniawanx@gmail.com>
2022-12-20 15:44:36 +0700

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Fragment } from "react";
+import { Title, Wrapper, Button, TomatoButton, Rotate } from "./sytled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+      <div>
+        <Button>Normal Button</Button>
+        <Button as="a" href="#">
+          Link with Button styles
+        </Button>
+        <TomatoButton as="a" href="#">
+          Link with Tomato Button styles
+        </TomatoButton>
+      </div>
+      <Rotate>&lt; 💅🏾 &gt;</Rotate>
+    </Fragment>
   );
 }
-
 export default App;
---
+++ src/sytled.js
@@ -0,0 +1,51 @@
+import styled, { keyframes } from "styled-components";
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+// Use Title and Wrapper like any other React component – except they're styled!
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+// Create the keyframes
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+// Here we create a component that will rotate everything we pass in over two seconds
+const Rotate = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+`;
+
+export { Title, Wrapper, Button, TomatoButton, Rotate };
